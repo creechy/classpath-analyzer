5da21bda64cc382d5f3cf1c47b8766a9734be935
Joseph Mocker <mock@fakebelieve.org>
2011-11-14 07:48:30 -0800

    Added --classes-only and --summary options to classpath checking.

--- src/org/fakebelieve/classpath/analyzer/Analyzer.java
+++ src/org/fakebelieve/classpath/analyzer/Analyzer.java
@@ -6,10 +6,10 @@ package org.fakebelieve.classpath.analyzer;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -27,7 +27,7 @@ public class Analyzer {
     /**
      * @param args the command line arguments
      */
-    public void checkClasspath(String[] args) throws IOException {
+    public void checkClasspath(List<String> args, boolean classesOnly, boolean summary) throws IOException {
         SortedSet<String> duplicates = new TreeSet<String>();
         Map<String, List<ClassInfo>> klasses = new HashMap<String, List<ClassInfo>>();
         for (String file : args) {
@@ -48,22 +48,33 @@ public class Analyzer {
             zf.close();
         }
 
+        Set<String> archiveSummary = new HashSet<String>();
         for (String duplicate : duplicates) {
-            if (!duplicate.endsWith("/")) {
-                System.out.println(duplicate);
+            if (!duplicate.endsWith("/") && (!classesOnly || (classesOnly && duplicate.endsWith(".class")))) {
+                if (!summary) {
+                    System.out.println(duplicate);
+                }
                 for (ClassInfo ce : klasses.get(duplicate)) {
-                    Date date = new Date(ce.getZe().getTime());
-                    System.out.println(String.format("  -> %016x %tF %tR %s", ce.getZe().getCrc(), date, date, ce.getLocation()));
+                    if (summary) {
+                        if (archiveSummary.contains(ce.getLocation())) {
+                            continue;
+                        }
+                        System.out.println(ce.getLocation());
+                        archiveSummary.add(ce.getLocation());
+                    } else {
+                        Date date = new Date(ce.getZe().getTime());
+                        System.out.println(String.format("  -> %016x %tF %tR %s", ce.getZe().getCrc(), date, date, ce.getLocation()));
+                    }
                 }
             }
         }
     }
 
-    public void compareClasspath(String[] args) throws IOException {
+    public void compareClasspath(List<String> paths) throws IOException {
 
         List<SortedSet<String>> klassesInArchives = new ArrayList<SortedSet<String>>();
 
-        for (String file : args) {
+        for (String file : paths) {
             ZipFile zf = new ZipFile(file);
             Enumeration entries = zf.entries();
 
@@ -77,7 +88,7 @@ public class Analyzer {
             zf.close();
         }
 
-        compareTwo(args[0], klassesInArchives.get(0), args[1], klassesInArchives.get(1));
+        compareTwo(paths.get(0), klassesInArchives.get(0), paths.get(1), klassesInArchives.get(1));
     }
 
     public void compareTwo(String f1, SortedSet<String> s1, String f2, SortedSet<String> s2) {
@@ -107,9 +118,12 @@ public class Analyzer {
 
     public static void usage() {
         System.err.println("usage: java " + Analyzer.class.getName() + " \\");
-        System.err.println("\t--paths file1.jar ... fileN.jar | \\");
-        System.err.println("\t--compare file1.jar file2.jar");
-        System.exit(-1);
+        System.err.println(
+                "\t--paths file1.jar ... fileN.jar | \\");
+        System.err.println(
+                "\t--compare file1.jar file2.jar");
+        System.exit(
+                -1);
     }
 
     public static void main(String[] args) throws IOException {
@@ -118,11 +132,29 @@ public class Analyzer {
         }
 
         if (args[0].equals("--paths")) {
-            new Analyzer().checkClasspath(Arrays.copyOfRange(args, 1, args.length));
+            List<String> paths = new ArrayList<String>();
+            boolean classesOnly = false;
+            boolean summary = false;
+            for (int idx = 1; idx < args.length; idx++) {
+                if (args[idx].equals("--classes-only")) {
+                    classesOnly = true;
+                    continue;
+                }
+                if (args[idx].equals("--summary")) {
+                    summary = true;
+                    continue;
+                }
+                paths.add(args[idx]);
+            }
+            new Analyzer().checkClasspath(paths, classesOnly, summary);
         } else if (args[0].equals("--compare") && args.length == 3) {
-            new Analyzer().compareClasspath(Arrays.copyOfRange(args, 1, args.length));
+            List<String> paths = new ArrayList<String>();
+            for (int idx = 1; idx < args.length; idx++) {
+                paths.add(args[idx]);
+            }
+            new Analyzer().compareClasspath(paths);
+        } else {
+            usage();
         }
-
-        usage();
     }
 }
